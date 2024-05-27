6f8cde0b0830879327a9db73f0f55b11b0769e31
projekt1701 <tassilo.g@gmx.net>
2024-05-27 22:02:10 +0200

    creaete backend and frontend module

---
+++ backend/src/test/java/com/mitchris/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mitchris.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
